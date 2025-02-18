e6d8e1e4c67df952395305d06d30ae3789869b7e
SergeyEn76 <107599135+SergeyEn76@users.noreply.github.com>
2025-02-18 22:21:11 +0700

    Update task.js
    little code upgrate

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -2,14 +2,14 @@ function countCliker() {
     let image = document.getElementById("cookie");
     image.onclick = function() {
         const count = document.getElementById("clicker__counter");
-        count.textContent++;
-        if (parseFloat(count.textContent) % 2 != 0) {
-            image.width += 20;
-            image.height += 20;
-        } else {
-            image.width -= 20;
-            image.height -= 20;
-        }
+        image.width = ++count.textContent %2 ? 250: 200;
+//        if (parseFloat(count.textContent) % 2 != 0) {
+//            image.width += 20;
+//            image.height += 20;
+//        } else {
+//            image.width -= 20;
+//            image.height -= 20;
+//        }
     }
 }
 
