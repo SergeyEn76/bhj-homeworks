697c682c513ffcd824729015db00a03a0bb19f61
Sergey En <enyuhin@yandex.ru>
2025-04-03 22:15:00 +0700

    second commit todo - work on bugs

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -1,52 +1,54 @@
 let form = document.forms.tasks__form;
 let taskList = document.querySelector('.tasks__list');
-let tasks = Array.from(document.querySelectorAll('.task__remove'));
+let tasksArray = Array.from(document.querySelectorAll('.task__remove'));
 let storageTasks = localStorage.getItem('task');
+//localStorage.removeItem('task');
 
-if (!storageTasks) {
-    console.log('storage empty');    
-} else {
-    console.log('we have storage');
-    let tempStorage = storageTasks.split(';');
-    for (let element of tempStorage) {
-        let lastSymbol = element.substring(element.length - 1, element.length);
-        if (lastSymbol === '×') {
-            element = element.substr(0, element.length - 1);
-        }
-        taskList.insertAdjacentHTML('beforeend', `<div class="task"><div class="task__title">${element}</div><a href="#" class="task__remove">&times;</a></div>`);
+function tasksStorage() {
+    //добавление задач в localstorage
+    let tempArray = [];
+    for (let element of tasksArray) {
+        tempArray.push(element.textContent);
     }
-    tasks = Array.from(document.querySelectorAll('.task'));
-    chekForDelette();
+    localStorage.setItem('task', tempArray);
 }
 
-function chekForDelette() {
-    for (let element of tasks) {
+function tasks() {
+    //проверка наличия задач в localstorage и вывод на экран
+    if (storageTasks !== null && storageTasks !== '') {
+        let tempStorage = storageTasks.split(',');
+        for (let element of tempStorage) {
+            let lastSymbol = element.substring(element.length - 1, element.length);
+            if (lastSymbol === '×') {
+                element = element.substring(0, element.length - 1);
+            }
+            taskList.insertAdjacentHTML('beforeend', `<div class="task"><div class="task__title">${element}</div><a href="#" class="task__remove">&times;</a></div>`);
+            tasksArray = Array.from(document.querySelectorAll('.task'));
+        }
+    }
+    
+    //добавление задачи
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+        let newTask = form.elements['task__input'].value;
+        if (newTask.trim() === '') {
+            form.reset();
+            return;
+        }
+        taskList.insertAdjacentHTML('beforeend', `<div class="task"><div class="task__title">${newTask}</div><a href="#" class="task__remove">&times;</a></div>`);
+        tasksArray = Array.from(document.querySelectorAll('.task'));
+        tasksStorage();
+    });
+
+    //удаление задачи
+    for (let element of tasksArray) {
         element.addEventListener('click', (event) => {
             event.preventDefault();
             element.remove();
-            tasks = Array.from(document.querySelectorAll('.task'));
+            tasksArray = Array.from(document.querySelectorAll('.task'));
             tasksStorage();
         })
     }
-    tasksStorage();
 }
 
-function tasksStorage() {
-    let tempArray = [];
-    for (let element of tasks) {
-        tempArray.push(element.textContent);
-    }
-    let forStorage = tempArray.join(';');
-    localStorage.setItem('task', forStorage);
-}
-
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    let newTask = form.elements['task__input'].value;
-    if (newTask === '')
-        return;
-    taskList.insertAdjacentHTML('beforeend', `<div class="task"><div class="task__title">${newTask}</div><a href="#" class="task__remove">&times;</a></div>`);
-    form.reset();
-    tasks = Array.from(document.querySelectorAll('.task'));
-    chekForDelette();
-});
+tasks();
