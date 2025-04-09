c9d0187041095c733f629ab1b51a0db49df394a0
Sergey En <enyuhin@yandex.ru>
2025-04-10 00:20:56 +0700

    second commit task auth fix some bugs

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -1,25 +1,33 @@
 const formToSend = document.forms.signin__form;
+let chekedUser = localStorage.getItem('user');
 
-formToSend.addEventListener('submit', (event) => {
-    event.preventDefault();
-    
-    const xhr = new XMLHttpRequest();
+if (chekedUser === '') {
+    formToSend.addEventListener('submit', (event) => {
+        event.preventDefault();
+        
+        const xhr = new XMLHttpRequest();
 
-    xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/auth');
+        xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/auth');
 
-    const formData = new FormData(formToSend);
-    
-    xhr.send(formData);
+        const formData = new FormData(formToSend);
 
-    xhr.onreadystatechange = function () {
-        if(xhr.readyState === 4) {
-            let receivedText = JSON.parse(xhr.responseText);
-            if (receivedText.success == true) {
+        xhr.responseType = 'json';
+        
+        xhr.send(formData);
+
+        xhr.onload = () => {
+            let receivedText = xhr.response;
+            if (receivedText.success === true) {
                 document.querySelector('.welcome').classList.add('welcome_active');
                 document.getElementById('user_id').textContent = receivedText.user_id;
+                localStorage.setItem('user', receivedText.user_id);
+            } else {
+                localStorage.setItem('user','');
             }
+           formToSend.reset();
         };
-    };
-
-    formToSend.reset();
-});
+    });
+} else {
+    document.querySelector('.welcome').classList.add('welcome_active');
+    document.getElementById('user_id').textContent = chekedUser;
+}
